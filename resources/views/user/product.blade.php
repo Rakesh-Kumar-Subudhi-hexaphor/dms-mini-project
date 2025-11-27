1056dd1d50d5d48550682d699d43c9c9a60c1507
Rakesh-Kumar-Subudhi-hexaphor <rakesh.hexaphor@gmail.com>
2025-11-27 14:50:51 +0530

    symbol correction

--- resources/views/user/product.blade.php
+++ resources/views/user/product.blade.php
@@ -86,7 +86,7 @@
                                         </p>
 
                                         <div class="product-footer">
-                                            <span class="product-price">₹G{{ $product->price }}</span>
+                                            <span class="product-price">₹{{ $product->price }}</span>
 
                                             <!-- Button to open modal -->
                                             <button class="order-btn" data-bs-toggle="modal"
